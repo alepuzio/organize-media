423616a0361ebf7f697b4c6644bda127ad9699a6
Alessandro Puzielli <a3puzielli@gmail.com>
2020-07-16 15:14:16 +0200

    rimossi test non isolati

--- tests/TestSuiteProject-A-L.py
+++ tests/TestSuiteProject-A-L.py
@@ -1,13 +1,19 @@
 import unittest 
 
-from AbsolutePath import TestAbsolutePath
+#from AbsolutePath import TestAbsolutePath
 from AsString import TestAsString
 from Control import TestControl
 from Extension import TestExtension
 from Filename import TestFilename
 from GroupDirectory import TestGroupDirectory
-from GroupOriginalFiles import TestGroupOriginalFiles
+#from GroupOriginalFiles import TestGroupOriginalFiles
 from GroupReadFiles import TestGroupReadFiles
+from ImageJPG import TestImageJPG
+from Field import TestField
+
+
+
+
 
 
 class ConfigTestCase(unittest.TestCase):
