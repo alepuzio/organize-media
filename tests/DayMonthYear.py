f6e8345bed0ec16f7764a30b35b021b066b3bd69
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-08-06 15:06:08 +0200

    corrected test and decorator

--- tests/DayMonthYear.py
+++ tests/DayMonthYear.py
@@ -8,24 +8,52 @@ class DayMonthYear:
         self.month = newmonth
         self.day = newday
 
-    def show(self):
-        '''@return creation year as 'dd/mm/yyyy' '''
-        return "/" .join ( [self.day, self.month , self.year] )
+    def show(self, separator):
+        '''@return creation year as 'ddseparatormmseparatoryyyy'
+           @param separator = string
+        '''
+        return separator.join ( [self.day, self.month , self.year] )
 
     def __repr__(self):
         return "DayMonthYear[" + self.day +"][" + self.month +  "][" + self.year + "]"
 
+class Slash:
+    '''@overview: decorator with slash'''
+    def __init__(self, newdaymonthyear):
+        self.daymonthyear = newdaymonthyear
+
+    def show(self):
+        return self.daymonthyear.show("/")
+
+
+class Space:
+    '''@overview: decorator with one space'''
+    def __init__(self, newdaymonthyear):
+        self.daymonthyear = newdaymonthyear
+
+    def show(self):
+        return self.daymonthyear.show(" ")
+
+
+
+
 
 class TestDayYearMonth(unittest.TestCase):
 
-    def test_show(self):
-        time = "Wed Jun 10 17:04:28 2020"
-        year = Year(time)
-        month = Month (time)
-        day = Day(time)
-        dayMonthYear = DayMonthYear(day, month, year)
+    def test_show_slash(self):
+        year = "2020"
+        month = "06"
+        day = "10"
+        dayMonthYear = Slash(DayMonthYear(day, month, year)).show()
         expected = "10/06/2020"
-        self.assertEqual( dayMonthYear.show(), expected)
+        self.assertEqual( dayMonthYear, expected)
 
 
+    def test_show_space(self):
+        year = "2020"
+        month = "06"
+        day = "10"
+        dayMonthYear = Space(DayMonthYear(day, month, year)).show()
+        expected = "10 06 2020"
+        self.assertEqual( dayMonthYear, expected)
 
