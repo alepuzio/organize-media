9d612eaea6d2414e39bb526865d64241cdd76520
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-09 11:26:57 +0100

    fix test about media

---
+++ tests/test_media.py
@@ -0,0 +1,36 @@
+from organizemedia.personal_logging import PersonalLogging
+import pytest
+from .test_extension import Extension
+
+
+class Media:
+    '''@overview this class extract the type of media'''
+
+    def __init__(self, newextension):
+        self.extension = newextension
+        self.log = PersonalLogging("Media", False)
+
+    def directory(self):
+        '''@return the name of the directory about the media'''
+        pieces = self.extension.name().split(".")
+        pieces.reverse();
+        return  pieces[0].upper() 
+
+    def __eq__(self, other):
+        return self.extension == other.extension
+
+    def __str__(self):
+        return "Media[" + str(self.extension) + "]"
+
+    def __repr__(self):
+        return "Media[" + str(self.extension) + "]->[" + self.directory() +"]"
+
+#class TestMedia(unittest.TestCase):
+
+def test_media():
+    extension = Extension("generic_image_raw.CR2")
+    result = Media(extension).directory()
+    expected = "CR2"
+    assert (result == expected)
+
+
