7774301b669d85639007144ea6dfb68667ef7f7e
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-12-30 15:56:05 +0100

    aded class of directory

---
+++ tests/test_original_root.py
@@ -0,0 +1,14 @@
+
+class Original_Root:
+    """@overview: class that represents the read directory with the last subdirectory as  Topic of the files
+    """
+
+
+class Test_Original_Root:
+    
+class Final_Root:
+    """@overview: class that represents the absolute directory where copy the organized files 
+    """
+
+
+class Test_Final_Root:
